0ba54c96e81e26563a49e7ba0c7796e850ad749e
SamaAshrafAhmed <132449394+SamaAshrafAhmed@users.noreply.github.com>
2024-10-06 19:25:58 +0300

    added destinations to nav controller

--- app/src/main/java/com/example/studyflash/ui/navigation/Nav.kt
+++ app/src/main/java/com/example/studyflash/ui/navigation/Nav.kt
@@ -3,11 +3,17 @@ package com.example.studyflash.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.studyflash.ui.screens.Add_Edit_Card_Screen
+import com.example.studyflash.ui.screens.Add_Edit_Category
+import com.example.studyflash.ui.screens.CardsListScreen
 import com.example.studyflash.ui.screens.CategoriesScreen
 import com.example.studyflash.ui.screens.HomeScreen
+import com.example.studyflash.ui.screens.IndividualCardScreen
 import com.example.studyflash.ui.screens.ProfileScreen
 
 @Composable
@@ -25,6 +31,44 @@ fun NavHost(
         }
         composable("profile") {
             ProfileScreen(navController)
+        }
+
+
+        
+        composable("addCategory") {
+            Add_Edit_Category()
+        }
+        composable("addCard") {
+            Add_Edit_Card_Screen()
+        }
+        composable("editCategory/{catId}", arguments = listOf(
+            navArgument("categId") {
+                NavType.IntType
+            }
+        )) {
+            Add_Edit_Category()
+        }
+        composable("editCard/{cardId}",arguments = listOf(
+            navArgument("cardId") {
+                NavType.IntType
+            }
+        )) {
+            Add_Edit_Card_Screen()
+        }
+
+        composable("individual Card/{cardId}", arguments = listOf(
+            navArgument("cardId") {
+                NavType.IntType
+            }
+        )) {
+            IndividualCardScreen()
+        }
+        composable("Cards List/{categId}", arguments = listOf(
+            navArgument("categId") {
+                NavType.IntType
+            }
+        )) {
+            CardsListScreen()
         }
     }
 }
