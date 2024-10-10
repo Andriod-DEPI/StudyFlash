47f6589a988a97b2b0c3634ee12f8735fb63b059
SamaAshrafAhmed <132449394+SamaAshrafAhmed@users.noreply.github.com>
2024-10-10 08:52:47 +0300

    updated main activity

--- app/src/main/java/com/example/studyflash/MainActivity.kt
+++ app/src/main/java/com/example/studyflash/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.studyflash.ui.theme.StudyFlashTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,31 +12,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            StudyFlashTheme {
+                // Create a NavController instance
+                val navController = rememberNavController()
 
-//            StudyFlashTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
+
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StudyFlashTheme {
-        Greeting("Android")
     }
 }
