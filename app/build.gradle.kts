af7e711ab5a7410f0381f909d57ac6882198c0f4
SamaAshrafAhmed <132449394+SamaAshrafAhmed@users.noreply.github.com>
2024-10-11 12:37:16 +0300

    added hilt dependencies for jetpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,11 @@ dependencies {
     //hilt dependency injection
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("androidx.hilt:hilt-work:1.0.0")
+    // When using Kotlin.
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
+
 }
 kapt {
     correctErrorTypes = true
